0fbbbeb07797acf39be63584781b31e252bda81f
karl <13973372654@163.com>
2021-07-12 13:39:10 +0800

    init config

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import actions from './actions'
+import user from './modules/user'
+import mutations from './mutations'
+import state from './state'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state,
+  mutations,
+  actions,
   modules: {
+    user
   }
 })
